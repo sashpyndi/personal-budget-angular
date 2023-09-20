572dc52efa871facb1958164c20bf741ffffe7fc
Sashank Pyndi <70219849+sashpyndi@users.noreply.github.com>
2023-09-20 15:53:12 -0400

    Loading data from data file

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
+/*const budget = {
+  myBudget: [
+    {
+      title: "Eat out",
+      budget: 30,
+    },
+    {
+      title: "Rent",
+      budget: 350,
+    },
+    {
+      title: "Rent",
+      budget: 90,
+    },
+  ],
+};*/
+app.use("/", express.static("public"));
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/budget", (req, res) => {
+  const budget = require('./budget.json');
+  res.json(budget);
 
-app.use('/', express.static('public'));
-const budget = {
-    myBudget:[
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 100
-    },
-    {
-      title: 'Grocery',
-      budget: 375
-    },
-]};
-app.get('/hello', (req, res) =>{
-   res.send("Hello World!");
-})
-app.get('/budget', (req, res) =>{
-     res.json(budget);
-})
-app.listen(port, () =>{
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
